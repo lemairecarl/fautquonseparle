c235bc251c8824f9f6142d25e9e93a19eeb586c5
Carl Lemaire <c.lemaire@live.ca>
2017-06-05 18:16:50 -0400

    Highlight; Optimiser

--- tsneviewer/sketch.js
+++ tsneviewer/sketch.js
@@ -7,6 +7,8 @@ function setup() {
 
     createCanvas(windowWidth, windowHeight);
     textAlign(CENTER, CENTER);
+    rectMode(CENTER);
+    noStroke();
 
     //words = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
     //vecs = [[0, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2], [2, 0], [2, 1], [2, 2]];
@@ -26,17 +28,52 @@ function setup() {
 function draw() {
     background('white');
 
-    for(var i = 0; i < words.length; i++) {
+    closest = 0
+    closestDist = 9999
+    if (zoom < 0.5) {
+        skip = 8;
+    } else if (zoom < 1.0) {
+        skip = 4;
+    } else if (zoom < 2.0) {
+        skip = 2;
+    } else {
+        skip = 1;
+    }
+    for(var i = 0; i < words.length; i += skip) {
         windowCoord = worldToWindow(vecs[i]);
         if (isInsideWindow(windowCoord)) {
-            fill(colorPalette[q[i]]);
-            text(
-                words[i],
-                windowCoord[0],
-                windowCoord[1]
-                );
+            distToMouse = sqDist(mouseCoord(), windowCoord);
+            if (distToMouse < closestDist) {
+                closestDist = distToMouse;
+                closest = i;
+            }
+            drawWord(i);
         }
     }
+    drawWord(closest, true);
+}
+
+function drawWord(i, hover=false) {
+    windowCoord = worldToWindow(vecs[i]); //TODO optimiser
+    if (hover) {
+        fill('white');
+        tw = textWidth(words[i]);
+        rect(windowCoord[0], windowCoord[1], tw, textSize());
+        fill('black');
+    } else {
+        fill(colorPalette[q[i]]);
+    }
+    text(
+        words[i],
+        windowCoord[0],
+        windowCoord[1]
+        );
+}
+
+function sqDist(a, b) {
+    dx = a[0] - b[0];
+    dy = a[1] - b[1];
+    return dx * dx + dy * dy;
 }
 
 function isInsideWindow(coord) {
