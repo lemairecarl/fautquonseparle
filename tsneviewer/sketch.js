e9af7538879b57698bf894246661de64a518599f
Carl Lemaire <c.lemaire@live.ca>
2017-06-05 17:11:55 -0400

    Utiliser p5 hosté

--- tsneviewer/sketch.js
+++ tsneviewer/sketch.js
@@ -6,6 +6,8 @@ function setup() {
     colorPalette = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf'];
 
     createCanvas(windowWidth, windowHeight);
+    textAlign(CENTER, CENTER);
+
     //words = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
     //vecs = [[0, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2], [2, 0], [2, 1], [2, 2]];
     words = jsondata.words;
